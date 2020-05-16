5c6d8ed8247e58d9618c99b86c145ec75aa7608b
sigmade <sychyov1991@mail.ru>
2020-05-16 09:50:06 +0300

    Add project files.

---
+++ CSharpCarClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CarLibrary;
+
+namespace CSharpCarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** C# CarLibrary Client App ******");
+            // Create object SportsCar
+            SportsCar viper = new SportsCar("Viper", 240, 40);
+            viper.TurboBoost();
+
+            // Create object MiniVan
+            MiniVan mv = new MiniVan();
+            mv.TurboBoost();
+
+            Console.WriteLine("Done. Press any key to terminate");
+            Console.ReadLine();
+        }
+    }
+}
